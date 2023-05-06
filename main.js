71d0f5815674a971931d3cdd2996d471a7a3b64e
prachiwhitehatjr <123462997+prachiwhitehatjr@users.noreply.github.com>
2023-05-06 16:52:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 function preload(){
-
-}
-function setup (){
-canvas=createCanvas(300,300);
-canvas.center();
-}
-
-
-
-function draw(){
-
-}
+    clownnose=loadImage('https://i.postimg.cc/sshfQbg3/m.png');
+    }
+    function setup (){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+    }
+    function gotPoses(results){
+    if (results.length>0)
+    {
+        console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("nosex="+results[0].pose.nose.x);
+        console.log("nosey="+results[0].pose.nose.y);
+    }
+    
+    }
+    function draw(){
+    image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(250,0,0);
+    circle(nosex,nosey,20);
+    image(clownnose,nosex,nosey,30,30);
+    
+    }
+    function take_snapshot(){
+        save('clownnose.png');
+    }
+    function modelLoaded(){
+    console.log('postNetisInitialised');
+    
+    }
+    
